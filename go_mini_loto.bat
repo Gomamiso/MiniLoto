feea9ac0df9d57c6e547b22404131d298a65f7b9
Gomamiso <hitogit@gmail.com>
2023-09-17 13:56:06 +0900

    回と数字だけを得る

---
+++ go_mini_loto.bat
@@ -0,0 +1,12 @@
+goto load_html
+goto get_test_data
+
+:load_html
+python load_html.py
+goto exit
+
+:get_test_data
+python get_html_record.py -i ƒ~ƒjƒƒg_‰ß‹‚Ì’Š‚¹‚ñŒ‹‰Ê_*.html -o test_list.csv
+goto exit
+
+:exit
--- load_html.py
+++ load_html.py
@@ -3,48 +3,50 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = webdriver.ChromeOptions()
 
-# ウェブドライバーのパスを設定します（あなたのシステムによって異なります）
-#path_to_webdriver = 'C:/Program Files (x86)/Google/Chrome/Application/114.0.5735.90/chromedriver.exe'
-
 # ブラウザを起動
-#browser = webdriver.Chrome(executable_path=path_to_webdriver)
 browser = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 # ウェブページを開く
 url = "https://www.takarakujinet.co.jp/miniloto/past.html"
 browser.get(url)
 
-# JavaScriptがデータをロードするのを待つ（時間を調整するか、より洗練された待機方法を使用するかもしれません）
-time.sleep(10)
+# JavaScriptがデータをロードするのを待つ
+try:
+	# エレメントがロードされるまでの最大待機時間を指定（10秒）
+	element = WebDriverWait(browser, 10).until(
+		EC.presence_of_element_located((By.ID, "topics-list")) # ここに適切なIDや他の属性を指定
+	)
+finally:
+	# ページのHTMLを取得
+	html = browser.page_source
 
-# ページのHTMLを取得
-html = browser.page_source
+	# BeautifulSoupオブジェクトを作成
+	soup = BeautifulSoup(html, 'html.parser')
 
-# BeautifulSoupオブジェクトを作成
-soup = BeautifulSoup(html, 'html.parser')
-
-
-# ここでデータを抽出します（具体的な抽出方法は、ページの構造に依存します）
-# 例えば、テーブルのデータを抽出する場合のコード：
-key = 'result table-result-loto'
-table_list = soup.find_all('table', {'class': key})
-for ix, table in enumerate(table_list):
-	print(ix, table)
-
-'''
-table = soup.find('table', {'class': key})
-
-if table:
-    for row in table.find_all('tr')[1:]:
-        cols = row.find_all('td')
-        cols = [col.get_text(separator=' ').strip() for col in cols]
-        print(cols)
-else:
-    print("Table not found")
-'''
+	# ここでデータを抽出します
+	key = 'result table-result-loto'
+	table_list = soup.find_all('table', {'class': key})
+	for ix, table in enumerate(table_list):
+		for row in table.find_all('tr'):
+			cols = row.find_all('th', class_='title', colspan="4")
+			if cols != None and len(cols) > 0:
+				text = cols[0].get_text()[1:].strip()
+				match = re.search(r'第(\d+)回', text)
+				if match:
+					result = match.group(1)
+					print(ix,'title:',result)
+			cols = row.find_all('th', class_='lotnum')
+			if cols != None and len(cols) > 0:
+				text = cols[0].get_text()[1:].strip()
+				numbers = re.findall(r'\d+', text)
+				numbers_list = [int(num) for num in numbers]
+				print(ix,'lotnum:',numbers_list)
 
 # ブラウザを閉じる
 browser.quit()
